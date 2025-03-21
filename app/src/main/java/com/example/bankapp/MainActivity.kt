7f14d16109351dbf04683f31b35469d6cfcd0be5
PabloEmilioAlonsoRomero <157993849+PabloAlonsoRomero@users.noreply.github.com>
2025-03-21 16:49:22 -0600

    Iniciando estadisticas

--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.bankapp.components.Header
 import com.example.bankapp.ui.theme.BankAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +39,7 @@ fun HomeScreen(innerPadding : PaddingValues) {
             .padding(innerPadding)
             .padding(20.dp)
     ) {
-
+        Header()
     } // Fin columna
 }
 
